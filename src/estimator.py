Added Nd and Ng parameters to discriminator and generator, respectively. Fixed covariance/mean calculations in colour consistency regularization

src/estimator.py
@@ -74,25 +74,25 @@ def model_fn(features, labels, mode, params):
 
                 with tf.variable_scope('G1'):
                     L_G1  = losses.G_loss(D_G1)
-                    L_G1 += losses.colour_consistency_regularization(G1, G0)
+                    L_G1 += losses.colour_consistency_regularization(G1, G0, data_format=data_format)
 
                 with tf.variable_scope('G2'):
                     L_G2  = losses.G_loss(D_G2)
-                    L_G2 += losses.colour_consistency_regularization(G2, G1)
+                    L_G2 += losses.colour_consistency_regularization(G2, G1, data_format=data_format)
 
                 with tf.variable_scope('D0'):
                     L_D0   = losses.D_loss(D_R0, D_G0)
-                    L_D0_W = losses.wasserstein(R0, G0, discriminator.D0, D0_scope)
+                    L_D0_W = losses.wasserstein_loss(R0, G0, discriminator.D0, D0_scope)
                     L_D0  += L_D0_W
 
                 with tf.variable_scope('D1'):
                     L_D1   = losses.D_loss(D_R1, D_G1)
-                    L_D1_W = losses.wasserstein(R1, G1, discriminator.D1, D1_scope)
+                    L_D1_W = losses.wasserstein_loss(R1, G1, discriminator.D1, D1_scope)
                     L_D1  += L_D1_W
 
                 with tf.variable_scope('D2'):
                     L_D2   = losses.D_loss(D_R2, D_G2)
-                    L_D2_W = losses.wasserstein(R2, G2, discriminator.D2, D2_scope)
+                    L_D2_W = losses.wasserstein_loss(R2, G2, discriminator.D2, D2_scope)
                     L_D2  += L_D2_W
 
                 with tf.variable_scope('G'):
@@ -100,41 +100,39 @@ def model_fn(features, labels, mode, params):
 
         if mode == ModeKeys.TRAIN:
             with tf.variable_scope('optimizers'):
-                trainable_vars = tf.trainable_variables()
-                G_vars  = [var for var in trainable_vars if 'generator' in var.name]
-                D0_vars = [var for var in trainable_vars if 'discriminator/D0' in var.name]
-                D1_vars = [var for var in trainable_vars if 'discriminator/D1' in var.name]
-                D2_vars = [var for var in trainable_vars if 'discriminator/D2' in var.name]
+                with tf.control_dependencies([tf.assign_add(global_step, 1)]):
+                    trainable_vars = tf.trainable_variables()
+                    G_vars  = [var for var in trainable_vars if 'generator' in var.name]
+                    D0_vars = [var for var in trainable_vars if 'discriminator/D0' in var.name]
+                    D1_vars = [var for var in trainable_vars if 'discriminator/D1' in var.name]
+                    D2_vars = [var for var in trainable_vars if 'discriminator/D2' in var.name]
 
-                D0_train = create_train_op(L_D0,
-                                           global_step=D0_global_step,
-                                           learning_rate=D_lr,
-                                           var_list=D0_vars,
-                                           use_tpu=use_tpu)
+                    D0_train = create_train_op(L_D0,
+                                               global_step=D0_global_step,
+                                               learning_rate=D_lr,
+                                               var_list=D0_vars,
+                                               use_tpu=use_tpu)
 
-                D1_train = create_train_op(L_D1,
-                                           global_step=D1_global_step,
-                                           learning_rate=D_lr,
-                                           var_list=D1_vars,
-                                           use_tpu=use_tpu)
+                    D1_train = create_train_op(L_D1,
+                                               global_step=D1_global_step,
+                                               learning_rate=D_lr,
+                                               var_list=D1_vars,
+                                               use_tpu=use_tpu)
 
-                D2_train = create_train_op(L_D2,
-                                           global_step=D2_global_step,
-                                           learning_rate=D_lr,
-                                           var_list=D2_vars,
-                                           use_tpu=use_tpu)
+                    D2_train = create_train_op(L_D2,
+                                               global_step=D2_global_step,
+                                               learning_rate=D_lr,
+                                               var_list=D2_vars,
+                                               use_tpu=use_tpu)
 
-                with tf.control_dependencies([D2_train, D1_train, D0_train]):
-                    G_train = create_train_op(L_G,
-                                              global_step=G_global_step,
-                                              learning_rate=G_lr,
-                                              var_list=G_vars,
-                                              use_tpu=use_tpu)
+                    with tf.control_dependencies([D2_train, D1_train, D0_train]):
+                        G_train = create_train_op(L_G,
+                                                  global_step=G_global_step,
+                                                  learning_rate=G_lr,
+                                                  var_list=G_vars,
+                                                  use_tpu=use_tpu)
 
-                train_op = tf.group(G_train, D2_train, D1_train, D0_train)
-
-                with tf.control_dependencies([train_op]):
-                    tf.assign_add(global_step, 1)
+                    train_op = tf.group(G_train, D2_train, D1_train, D0_train)
 
         loss = L_G
 

src/model/discriminator.py
@@ -12,85 +12,86 @@ class StackGANDiscriminator():
             data_format  (str): The data format to use for the image.
         """
         self.data_format = data_format
+        self.Nd = 64
 
     def D0(self, Im0, scope=None):
         with slim.arg_scope([slim.conv2d, slim.batch_norm], data_format=self.data_format):
             with tf.variable_scope(scope or 'discriminator/D0', reuse=tf.AUTO_REUSE) as D0_scope:
-                output = self.encode_x16(Im0)
+                net = self.encode_x16(Im0)
 
-                logits = self.logits(output)
+                logits = self.logits(net)
 
                 return logits, D0_scope
 
     def D1(self, Im1, scope=None):
         with slim.arg_scope([slim.conv2d, slim.batch_norm], data_format=self.data_format):
             with tf.variable_scope(scope or 'discriminator/D1', reuse=tf.AUTO_REUSE) as D1_scope:
-                output = self.encode_x16(Im1)
-                output = self.downsample(output, 16)
-                output = self.conv3x3(output, 8)
+                net = self.encode_x16(Im1)
+                net = self.downsample(net, 16*self.Nd)
+                net = self.conv3x3(net, 8*self.Nd)
 
-                logits = self.logits(output)
+                logits = self.logits(net)
 
                 return logits, D1_scope
 
     def D2(self, Im2, scope=None):
         with slim.arg_scope([slim.conv2d, slim.batch_norm], data_format=self.data_format):
             with tf.variable_scope(scope or 'discriminator/D2', reuse=tf.AUTO_REUSE) as D2_scope:
-                output = self.encode_x16(Im2)
-                output = self.downsample(output, 16)
-                output = self.downsample(output, 32)
-                output = self.conv3x3(output, 16)
-                output = self.conv3x3(output, 8)
+                net = self.encode_x16(Im2)
+                net = self.downsample(net, 16*self.Nd)
+                net = self.downsample(net, 32*self.Nd)
+                net = self.conv3x3(net, 16*self.Nd)
+                net = self.conv3x3(net, 32*self.Nd)
 
-                logits = self.logits(output)
+                logits = self.logits(net)
 
                 return logits, D2_scope
 
-    def conv3x3(self, input_tensor, filters):
+    def conv3x3(self, net, filters):
         with tf.name_scope('conv3x3_block'):
-            output = slim.conv2d(input_tensor, filters, kernel_size=3, stride=1, padding='same')
-            output = slim.batch_norm(output)
-            output = tf.nn.leaky_relu(output)
-            return output
+            net = slim.conv2d(net, filters, kernel_size=3, stride=1, padding='same')
+            net = slim.batch_norm(net)
+            net = tf.nn.leaky_relu(net)
+            return net
 
-    def downsample(self, input_tensor, filters):
+    def downsample(self, net, filters):
         with tf.name_scope('downsample'):
-            output = slim.conv2d(input_tensor, filters, kernel_size=4, stride=2, padding='same', biases_initializer=None)
-            output = slim.batch_norm(output)
-            output = tf.nn.leaky_relu(output)
+            net = slim.conv2d(net, filters, kernel_size=4, stride=2, padding='same', biases_initializer=None)
+            net = slim.batch_norm(net)
+            net = tf.nn.leaky_relu(net)
 
-            return output
+            return net
 
-    def logits(self, input_tensor):
-        with tf.variable_scope('logits'):
-            output = slim.conv2d(input_tensor, 1, kernel_size=4, stride=4, padding='same')
-            output = tf.nn.sigmoid(output)
-            return tf.reshape(output, [-1])
+    def logits(self, net):
+        with tf.name_scope('logits'):
+            net = slim.conv2d(net, 1, kernel_size=4, stride=4, padding='same')
+            net = tf.nn.sigmoid(net)
+            return tf.reshape(net, [-1])
 
-    def add_noise(self, output):
+    def add_noise(self, net):
         with tf.name_scope('noise'):
-            noise  = tf.random_normal(tf.shape(output), stddev=0.02, dtype=tf.float32)
-            output = output + noise
-            return output
+            noise  = tf.random_normal(tf.shape(net), stddev=0.02, dtype=tf.float32)
+            net = net + noise
+            return net
 
-    def encode_x16(self, output, add_noise=False):
+    def encode_x16(self, net):
         with tf.name_scope('encode_x16'):
             with slim.arg_scope([slim.conv2d], kernel_size=4, stride=2, padding='same'):
-                output = self.add_noise(output)
+                net = self.add_noise(net)
 
-                output = slim.conv2d(output, 64)
-                output = tf.nn.leaky_relu(output)
+                net = slim.conv2d(net, self.Nd)
+                net = tf.nn.leaky_relu(net)
 
-                output = slim.conv2d(output, 128)
-                output = slim.batch_norm(output)
-                output = tf.nn.leaky_relu(output)
+                net = slim.conv2d(net, 2*self.Nd)
+                net = slim.batch_norm(net)
+                net = tf.nn.leaky_relu(net)
 
-                output = slim.conv2d(output, 256)
-                output = slim.batch_norm(output)
-                output = tf.nn.leaky_relu(output)
+                net = slim.conv2d(net, 4*self.Nd)
+                net = slim.batch_norm(net)
+                net = tf.nn.leaky_relu(net)
 
-                output = slim.conv2d(output, 512)
-                output = slim.batch_norm(output)
-                output = tf.nn.leaky_relu(output)
+                net = slim.conv2d(net, 8*self.Nd)
+                net = slim.batch_norm(net)
+                net = tf.nn.leaky_relu(net)
 
-                return output
+                return net

src/model/generator.py
@@ -13,6 +13,7 @@ class StackGANGenerator():
             data_format  (str): The data format to use for the image.
         """
         self.data_format = data_format
+        self.Ng = 32
 
     def __call__(self, z):
         """
@@ -30,73 +31,73 @@ class StackGANGenerator():
             with slim.arg_scope([slim.conv2d, slim.batch_norm], data_format=self.data_format):
 
                 with tf.variable_scope('FC'):
-                    output       = slim.fully_connected(z, 4*4*64, activation_fn=None)
-                    output_shape = [-1, 4, 4, 64] if self.data_format == 'NHWC' else [-1, 64, 4, 4]
-                    output       = slim.batch_norm(output)
-                    output       = tf.nn.leaky_relu(output)
-                    output       = tf.reshape(output, output_shape)
+                    net       = slim.fully_connected(z, 4*4*64*self.Ng, activation_fn=None)
+                    net_shape = [-1, 4, 4, 64*self.Ng] if self.data_format == 'NHWC' else [-1, 64*self.Ng, 4, 4]
+                    net       = slim.batch_norm(net)
+                    net       = tf.nn.leaky_relu(net)
+                    net       = tf.reshape(net, net_shape)
 
-                G0, output = self.G0(output)
-                G1, output = self.G1(output, z)
-                G2, output = self.G2(output, z)
+                G0, net = self.G0(net)
+                G1, net = self.G1(net, z)
+                G2, net = self.G2(net, z)
 
                 return G0, G1, G2, G_scope
 
-    def G0(self, output):
+    def G0(self, net):
         with tf.variable_scope('G0'):
-            output = self.upscale(output, [-1, 8, 8, 32])
-            output = self.upscale(output, [-1, 16, 16, 16])
-            output = self.upscale(output, [-1, 32, 32, 8])
-            output = self.upscale(output, [-1, 64, 64, 4])
-            G0     = tf.nn.tanh(self.upscale(output, [-1, 64, 64, 3], leaky_relu=False, batch_norm=False))
-            return G0, output
+            net = self.upscale(net, [-1, 8, 8,   32*self.Ng])
+            net = self.upscale(net, [-1, 16, 16, 16*self.Ng])
+            net = self.upscale(net, [-1, 32, 32, 8*self.Ng])
+            net = self.upscale(net, [-1, 64, 64, 4*self.Ng])
+            G0  = tf.nn.tanh(self.upscale(net, [-1, 64, 64, 3], leaky_relu=False, batch_norm=False))
+            return G0, net
 
-    def G1(self, output, z):
+    def G1(self, net, z):
         with tf.variable_scope('G1'):
-            output = self.joint_conv(output, z, 64)
-            output = self.residual_block(output, 64)
-            output = self.residual_block(output, 64)
-            output = self.upscale(output, [-1, 128, 128, 2])
-            G1     = tf.nn.tanh(self.upscale(output, [-1, 128, 128, 3], leaky_relu=False, batch_norm=False))
-            return G1, output
+            net = self.joint_conv(net, z, 64)
+            net = self.residual_block(net, 64)
+            net = self.residual_block(net, 64)
+            net = self.upscale(net, [-1, 128, 128, 2*self.Ng])
+            G1  = tf.nn.tanh(self.upscale(net, [-1, 128, 128, 3], leaky_relu=False, batch_norm=False))
+            return G1, net
 
-    def G2(self, output, z):
+    def G2(self, net, z):
         with tf.variable_scope('G2'):
-            output = self.joint_conv(output, z, 32)
-            output = self.residual_block(output, 32)
-            output = self.residual_block(output, 32)
-            output = self.upscale(output, [-1, 256, 256, 1])
-            G2     = tf.nn.tanh(self.upscale(output, [-1, 256, 256, 3], leaky_relu=False, batch_norm=False))
-            return G2, output
+            net = self.joint_conv(net, z, 32)
+            net = self.residual_block(net, 32)
+            net = self.residual_block(net, 32)
+            net = self.upscale(net, [-1, 256, 256, 1*self.Ng])
+            G2  = tf.nn.tanh(self.upscale(net, [-1, 256, 256, 3], leaky_relu=False, batch_norm=False))
+            return G2, net
 
-    def joint_conv(self, input_tensor, z, filters):
+    def joint_conv(self, net, z, filters):
         with tf.name_scope('joint_conv'):
-            input_shape = input_tensor.get_shape().as_list()
+            net_shape = net.get_shape().as_list()
 
             if self.data_format == 'NCHW':
-                channels = input_shape[1]
-                height   = input_shape[2]
-                width    = input_shape[3]
+                channels = net_shape[1]
+                height   = net_shape[2]
+                width    = net_shape[3]
 
                 z = tf.expand_dims(z, -1)
                 z = tf.expand_dims(z, -1)
                 z = tf.tile(z, [1, 1, height, width])
-                output = tf.concat([input_tensor, z], 1)
+                net = tf.concat([net, z], 1)
             else:
-                height   = input_shape[1]
-                width    = input_shape[2]
-                channels = input_shape[3]
+                height   = net_shape[1]
+                width    = net_shape[2]
+                channels = net_shape[3]
 
                 z = tf.expand_dims(z, 1)
                 z = tf.expand_dims(z, 1)
                 z = tf.tile(z, [1, height, width, 1])
-                output = tf.concat([input_tensor, z], -1)
+                net = tf.concat([net, z], -1)
 
-            output = slim.conv2d(output, filters, kernel_size=3, stride=1, padding='same')
-            output = slim.batch_norm(output)
-            output = tf.nn.leaky_relu(output)
+            net = slim.conv2d(net, filters, kernel_size=3, stride=1, padding='same')
+            net = slim.batch_norm(net)
+            net = tf.nn.leaky_relu(net)
 
-            return output
+            return net
 
     def residual_block(self, x, filters):
         with tf.name_scope('residual_block'):
@@ -114,7 +115,7 @@ class StackGANGenerator():
                 return tf.nn.leaky_relu(Fxx)
 
     def upscale(self,
-                 input_tensor,
+                 net,
                  output_shape,
                  kernel_size=3,
                  stride=1,
@@ -126,47 +127,44 @@ class StackGANGenerator():
         width   = output_shape[2]
 
         with tf.name_scope('upsample_%d_%d_%d' % (height, width, filters)):
-            output = self.resize(input_tensor, [height, width])
+            net = self.resize(net, [height, width])
 
-            output = slim.conv2d(output, filters,
-                                 kernel_size=kernel_size,
-                                 stride=stride,
-                                 padding='same')
+            net = slim.conv2d(net, filters,
+                              kernel_size=kernel_size,
+                              stride=stride,
+                              padding='same')
 
             if batch_norm:
-                output = slim.batch_norm(output)
+                net = slim.batch_norm(net)
             if leaky_relu:
-                output = tf.nn.leaky_relu(output)
-            return output
+                net = tf.nn.leaky_relu(net)
+            return net
 
-    def resize(self, input_tensor, dims):
+    def resize(self, net, dims):
         height = dims[0]
         width  = dims[1]
 
-        input_tensor_shape = input_tensor.get_shape().as_list()
+        net_shape = net.get_shape().as_list()
 
         if self.data_format == 'NCHW':
-            input_tensor_height = input_tensor_shape[2]
-            input_tensor_width = input_tensor_shape[3]
+            net_height = net_shape[2]
+            net_width = net_shape[3]
         else:
-            input_tensor_height = input_tensor_shape[1]
-            input_tensor_width = input_tensor_shape[2]
+            net_height = net_shape[1]
+            net_width = net_shape[2]
 
-        is_same_height_width = input_tensor_height == height and input_tensor_width == width
+        is_same_height_width = net_height == height and net_width == width
 
         if is_same_height_width:
-            return input_tensor
+            return net
 
         with tf.name_scope('resize'):
             if self.data_format == 'NCHW':
-                input_tensor = tf.transpose(input_tensor, [0, 2, 3, 1])
+                net = tf.transpose(net, [0, 2, 3, 1])
 
-            output  = tf.image.resize_nearest_neighbor(
-                input_tensor,
-                (height, width)
-            )
+            net  = tf.image.resize_nearest_neighbor(net, (height, width))
 
             if self.data_format == 'NCHW':
-                output = tf.transpose(output, [0, 3, 1, 2])
+                net = tf.transpose(net, [0, 3, 1, 2])
 
-            return output
+            return net

src/model/losses.py
@@ -2,65 +2,77 @@ import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
 def G_loss(G_logits):
-    return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=G_logits, labels=tf.ones_like(G_logits)))
+    return -tf.reduce_mean(tf.log(G_logits))
 
 def D_loss(D_logits, G_logits):
-    return tf.reduce_mean(
-        tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logits, labels=tf.ones_like(D_logits))
-        +tf.nn.sigmoid_cross_entropy_with_logits(logits=G_logits, labels=tf.ones_like(G_logits))
-    )
+    output  = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=D_logits, labels=true_labels(D_logits)))
+    output += tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=G_logits, labels=false_labels(G_logits)))
+    return output
+
+def false_labels(labels):
+    return tf.random_uniform(tf.shape(labels), .0, .3)
+
+def true_labels(labels):
+    return tf.random_uniform(tf.shape(labels), .8, 1.2)
 
 def interpolates(real_batch, fake_batch):
-    with tf.variable_scope('interpolates'):
-        real_batch = slim.flatten(real_batch)
-        fake_batch = slim.flatten(fake_batch)
-
+    with tf.name_scope('interpolates'):
         alpha = tf.random_uniform([tf.shape(real_batch)[0], 1], minval=0., maxval=1.)
-
         differences  = fake_batch - real_batch
         return real_batch + (alpha*differences)
 
 def lambda_gradient_penalty(logits, diff):
-    with tf.variable_scope('lambda_gradient_penalty'):
+    with tf.name_scope('lambda_gradient_penalty'):
         gradients = tf.gradients(logits, [diff])[0]
+        gradients = slim.flatten(gradients)
         slopes    = tf.sqrt(tf.reduce_sum(tf.square(gradients), reduction_indices=[1]))
         gradient_penalty = tf.reduce_mean((slopes-1.)**2)
 
         return 10*gradient_penalty
 
-def wasserstein(real_batch, fake_batch, discrim_func, discrim_scope):
+def wasserstein_loss(real_batch, fake_batch, discrim_func, discrim_scope):
+    return 0
+
     with tf.name_scope('wasserstein_loss'):
         diff = interpolates(real_batch, fake_batch)
-        diff_reshaped = tf.reshape(diff, tf.shape(real_batch))
-        interp_logits, _ = discrim_func(diff_reshaped, discrim_scope)
+        interp_logits, _ = discrim_func(diff, discrim_scope)
 
         return lambda_gradient_penalty(interp_logits, diff)
 
-def colour_consistency_regularization(G1, G0):
+def image_mean(img):
+    with tf.name_scope('image_mean'):
+        img_shape = img.get_shape().as_list()
+        channels  = img_shape[1]
+        pixels    = img_shape[2] * img_shape[3]
+
+        mu = tf.reduce_mean(img, [2, 3], keepdims=True)
+        img_mu = tf.reshape(img - mu, [-1, channels, pixels])
+        return mu, img_mu, pixels
+
+def image_covariance(img_mu, pixels):
+    with tf.name_scope('image_covariance'):
+        cov_matrix = tf.matmul(img_mu, img_mu, transpose_b=True)
+        cov_matrix = cov_matrix / pixels
+
+        return cov_matrix
+
+def colour_consistency_regularization(G1, G0, data_format):
     with tf.name_scope('cc_regularization'):
         lambda_1 = 1.0
         lambda_2 = 5.0
         alpha    = 50.0
 
-        G1       = slim.flatten(G1)
-        G0       = slim.flatten(G0)
+        if data_format == 'NHWC':
+            G0 = tf.transpose(G0, [0, 3, 1, 2])
+            G1 = tf.transpose(G1, [0, 3, 1, 2])
 
-        mu_si_j  = tf.reduce_mean(G1, -1)
-        mu_si1_j = tf.reduce_mean(G0, -1)
+        mu_si1_j, G0_mu, G0_pixels = image_mean(G0)
+        mu_si_j, G1_mu, G1_pixels  = image_mean(G1)
 
-        G1_mu = G1 - tf.expand_dims(mu_si_j, 1)
-        G0_mu = G0 - tf.expand_dims(mu_si1_j, 1)
+        cov_si1_j = image_covariance(G0_mu, pixels=G0_pixels)
+        cov_si_j  = image_covariance(G1_mu, pixels=G1_pixels)
 
-        G0_cov_matrix = tf.matmul(G0_mu, G0_mu, transpose_b=True)
-        G0_cov_matrix = G0_cov_matrix / G0.get_shape().as_list()[1]
-
-        G1_cov_matrix = tf.matmul(G1_mu, G1_mu, transpose_b=True)
-        G1_cov_matrix = G1_cov_matrix / G1.get_shape().as_list()[1]
-
-        cov_si_j  = tf.reduce_mean(G1_cov_matrix, -1)
-        cov_si1_j = tf.reduce_mean(G0_cov_matrix, -1)
-
-        L_ci  = lambda_1 * tf.norm(mu_si_j - mu_si1_j)**2
-        L_ci += lambda_2 * tf.norm(cov_si_j - cov_si1_j)**2
+        L_ci  = lambda_1 * tf.losses.mean_squared_error(mu_si_j, mu_si1_j)
+        L_ci += lambda_2 * tf.losses.mean_squared_error(cov_si_j, cov_si1_j)
 
         return alpha * tf.reduce_mean(L_ci)